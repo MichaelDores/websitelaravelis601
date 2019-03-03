65127da8a7dd1ea6621295c555a3fec75750fa6e
MichaelDores <Dores#7690>
2019-03-03 15:44:07 -0500

    Added to homeand about pages

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,8 +8,26 @@
     <br>
     <div class="col-md-6">
         <h2 align="center">
-            Eli Manning Bio
+            Eli Manning NYG Bio
         </h2>
-    <p>Eli Manning entered the league in 2004.  He was drafted by the Sand Diego Charges but was immediately
-    traded to the New York Giants.</p>
+        <br>
+    <p><a href="https://en.wikipedia.org/wiki/Eli_Manning">Eli Manning</a> entered the league in 2004.  He was drafted by the Sand Diego Charges but was immediately
+    traded to the New York Giants.  Within the first few years of his career he had already reached the pinnacle after winning
+    his first Super Bowl.  A few years later he won his second championship and cemented his legacy in NFL history.  In both Super Bowl
+    victories he was named the game's most valuable player.  Although Eli is approaching the end of his playing career,
+    he is still able to make an impact on the field and is greatly appreciated by the fans for what he's accomplished.</p>
+    </div>
+    <div class="col-md-6">
+        <h2 align="center">
+            Eli's Awards & Accolades
+        </h2>
+    <br>
+        <ul align="left">
+            <li style="text-align:left">2× Super Bowl Champion (XLII, XLVI)</li>
+            <li style="text-align:left">2× Super Bowl MVP (XLII, XLVI)</li>
+            <li style="text-align:left">4× Pro Bowl (2008, 2011, 2012, 2015)</li>
+            <li style="text-align:left">Most passing yards in a single postseason (1,219 yards, 2011)</li>
+            <li style="text-align:left">NFC passing touchdowns co-leader (2015)</li>
+        </ul>
+    </div>
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('content')
     <br>
-    <div class="jumbotron">
+    <div class="jumbotron jumbotron-fluid">
         <h1 align="center"><bold><ins>The New York Football Giants</ins></bold></h1>
     </div>
     <br>
@@ -11,7 +11,7 @@
             Giants!!!
         </h2>
         <br>
-    <p>The New York Giants were founded in 1925 by Tim Mara.  They play in the NFC East division along with the Dallas Cowboys, Philadelphia Eagles and Washington Reskins.
+    <p><a href="https://www.giants.com/">The New York Giants</a> were founded in 1925 by Tim Mara.  They play in the NFC East division along with the Dallas Cowboys, Philadelphia Eagles and Washington Reskins.
     In the NFL era the Giants have won the Super Bowl four times, most recently in 2011 against the New England Patriots.  Since their last Super Bowl victory, the Giants have struggled
     to make an impact resulting in coaching changes and player trades.  Their current coach, Pat Shurmur, is entering his second year with his sites set on bringing glory back to Big Blue.
     Along with his positive attitude and the plethora of players he has to choose from, there's reason to be optimistic.</p>
@@ -23,22 +23,19 @@
         <br>
         <iframe width="545" height="300" src="https://www.youtube.com/embed/hiYOB0wExvE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div class="col-sm-6">
+    <div class="col-md-6">
     <h2 align="center">Home of the Giants</h2>
-        <br>
-    <img src="http://www.dlrgroup.com/media/2967/58-11102-00_marquee-01.jpg" align="center" alt="Giants" width="375" height="175">
+    <img src="http://www.dlrgroup.com/media/2967/58-11102-00_marquee-01.jpg" class="img-fluid img-thumbnail" alt="Giants" width="525" height="300">
     </div>
-    <br>
-    <br>
-    <div class="col-4">
-        <br>
-        <br>
+    <div class="col-md-6">
         <h2 align="center">Stadium Stats</h2>
         <br>
-    <ul align="center">
-        <li style="text-align:center">Stadium Name - Metlife Stadium</li>
-        <li style="text-align:center">Inaugural Season - 2011</li>
-        <li style="text-align:center">Capacity - 82,500</li>
+    <ul align="left">
+        <li style="text-align:left">Stadium Name - Metlife Stadium</li>
+        <li style="text-align:left">Location - East Rutherford, New Jersey</li>
+        <li style="text-align:left">Inaugural Season - 2010</li>
+        <li style="text-align:left">Capacity - 82,500</li>
+        <li style="text-align:left">Tenants - New York Giants & New York Jets</li>
     </ul>
     </div>
 @stop
