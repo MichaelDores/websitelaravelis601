03c7aafe564964bb94fa60fcd154e5e86fdbcf59
MichaelDores <Dores#7690>
2019-03-04 09:49:24 -0500

    Added to about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <div class="jumbotron">
+    <div class="jumbotron jumbotron-fluid">
         <h1 align="center"><bold><ins>About Our Captain Eli Manning</ins></bold></h1>
     </div>
     <br>
@@ -17,34 +17,31 @@
     he is still able to make an impact on the field and is greatly appreciated by the fans for what he's accomplished.</p>
     </div>
     <div class="col-md-6">
-        <h2 align="center">
-            Eli's Awards & Accolades
-        </h2>
-    <br>
-        <ul align="left">
-            <li style="text-align:left">2× Super Bowl Champion (XLII, XLVI)</li>
-            <li style="text-align:left">2× Super Bowl MVP (XLII, XLVI)</li>
-            <li style="text-align:left">4× Pro Bowl (2008, 2011, 2012, 2015)</li>
-            <li style="text-align:left">Most passing yards in a single postseason (1,219 yards, 2011)</li>
-            <li style="text-align:left">NFC passing touchdowns co-leader (2015)</li></ul>
-    </div>
-    <div class="col-md-6">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
         <br>
         <img src="https://img.washingtonpost.com/rf/image_1484w/WashingtonPost/Content/Blogs/early-lead/Images/Super_Bowl_Football_0d300.jpg?uuid=rqW5IFC8EeG9T4p9U_bWwg" class="img-fluid img-thumbnail" align="right" alt="Giants" width="425" height="400">
     </div>
     <div class="col-md-6">
+        <br>
         <h2 align="center">
             The Highlight Machine
         </h2>
         <br>
-        <iframe align="left" width="530" height="275" src="https://www.youtube.com/embed/Hs5KWl5EOGQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p style="text-align:center;">The video above provides a glimpe into Eli's career and clearly displays his excellence</p>
+        <iframe align="center" width="530" height="275" src="https://www.youtube.com/embed/Hs5KWl5EOGQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    <div class="col-md-6">
         <br>
-        <p style="text-align:center;">The video above provides a glimpe into Eli's career and clearly displays how good he truly is.
-        He sure was good!!!</p>
+        <h2 align="center">
+            Eli's Awards & Accolades
+        </h2>
+        <br>
+        <ul>
+            <li>2× Super Bowl Champion (XLII, XLVI)</li>
+            <li>2× Super Bowl MVP (XLII, XLVI)</li>
+            <li>4× Pro Bowl (2008, 2011, 2012, 2015)</li>
+            <li>Most passing yards in a single postseason (1,219 yards, 2011)</li>
+            <li>NFC passing touchdowns co-leader (2015)</li>
+        </ul>
     </div>
 @stop
