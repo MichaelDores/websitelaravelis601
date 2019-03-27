f06e511df62aeb4b6cc4c8624afb24fe522c3d5d
MichaelDores <Dores#7690>
2019-03-27 12:54:59 -0400

    Changes made to thank you page/message

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.default')
 @section('content')
-    <h1>Thanks {{$name}}, we will get back to you shortly!!!</h1>
+    <br>
+    <h3>Thanks {{$name}}, we will get back to you shortly!!!</h3>
+    <br>
+    <br>
+    <br>
+    <img src="https://howigit.files.wordpress.com/2010/10/eli.jpg" class="img-fluid img-thumbnail" alt="Giants" width="400" height="175">
 @stop
