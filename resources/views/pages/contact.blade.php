bde803a6ac3215d010d85d8cb58ee035b96e7057
MichaelDores <Dores#7690>
2019-03-27 09:38:52 -0400

    Made changes to contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.default')
 @section('content')
     <div class="card">
-        <div class="card-title"><h1>Contact Us</h1>
+        <div class="card-title"><h1>Questions?</h1>
 
-        <p class ="lead">Questions?</p></div>
+        <p class ="lead">Contact Us</p></div>
 
 
 <form method="post" action=/contact role="form">
@@ -17,15 +17,15 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="form_name">First name *</label>
-                    <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your first name *" required="required" data-error="Firstname is required.">
+                    <label for="form_name">First name</label>
+                    <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your first name">
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="form_lastname">Last name *</label>
-                    <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your last name *" required="required" data-error="Lastname is required.">
+                    <label for="form_lastname">Last name</label>
+                    <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your last name">
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
@@ -33,15 +33,15 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="form_email">Email *</label>
-                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+                    <label for="form_email">Email</label>
+                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email">
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="form_need">Please specify your need *</label>
-                    <select id="form_need" name="need" class="form-control" required="required" data-error="Please specify your need.">
+                    <label for="form_need">Please specify your need</label>
+                    <select id="form_need" name="need" class="form-control">
                         <option value=""></option>
                         <option value="Request quotation">Additional NYG Info</option>
                         <option value="Request order status">Additional Eli Manning Info</option>
@@ -55,22 +55,16 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="form-group">
-                    <label for="form_message">Message *</label>
-                    <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please, leave us a message."></textarea>
+                    <label for="form_message">Message</label>
+                    <textarea id="form_message" name="message" class="form-control" placeholder="Message for me" rows="4"></textarea>
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
             <div class="col-md-12">
-                <input type="submit" class="btn btn-success btn-send" value="Send message">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="text-muted">
-                    <br>
-                    <strong>*</strong> These fields are required.</p>
+                <input type="submit" class="btn btn-success btn-send" value="Send message" style="background-color:red">
             </div>
         </div>
     </div>
 
 </form>
+        @stop
